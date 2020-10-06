defefdc00bf7c26afaa91bacff36405555585b79
Charlie Andrews <cwndrws@github.com>
2020-10-06 08:53:00 -0400

    Add install script for codespace setup

---
+++ install.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+main () {
+    install_system_deps
+    setup_zsh
+    setup_nvim
+}
+
+install_system_deps () {
+    local packages = "neovim zsh fzf"
+    apt install $packages
+}
+
+setup_zsh () {
+    install_oh_my_zsh
+    replace_zshrc
+}
+
+setup_nvim () {
+    install_pynvim
+    install_vim_plug
+    nvim_plugin_install
+}
+
+install_oh_my_zsh () {
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    chsh -s /usr/bin/zsh
+    /usr/bin/zsh
+}
+
+replace_zshrc () {
+    mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
+    source ~/.zshrc
+}
+
+install_pynvim () {
+    pip3 install pynvim
+}
+
+install_vim_plug () {
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+          https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+}
+
+nvim_plugin_install () {
+    nvim +UpdateRemotePlugins +PlugInstall +qall
+}
+
+if [ "$0" = "$BASH_SOURCE" ]; then
+    main
+fi
