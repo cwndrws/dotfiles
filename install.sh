211df9e3f4aaa61cf81d9db8c4ae385174352674
Charlie Andrews <cwndrws@github.com>
2021-05-18 11:43:20 -0400

    add simlinking of dotfiles to install script

--- install.sh
+++ install.sh
@@ -4,4 +4,11 @@ set -x
 
 echo "$(pwd)"
 
+ln -s .tmux.conf
+ln -s .tmuxline
+ln -s .vimrc $HOME/.vimrc
+ln -s .vim $HOME/.vim
+mkdir -p $HOME/.config
+ln -s .config/* $HOME/.config/
+
 vim +PlugInstall +q
