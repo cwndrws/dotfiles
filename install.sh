ee2494767809ff50151c78f3f5207e1dca4233ed
Charlie Andrews <cwndrws@github.com>
2021-05-18 12:00:54 -0400

    Add destinations for simlinks for tmux config and line

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ set -x
 
 echo "$(pwd)"
 
-ln -s .tmux.conf
-ln -s .tmuxline
+ln -s .tmux.conf $HOME/.tmux.conf
+ln -s .tmuxline $HOME/.tmuxline
 ln -s .vimrc $HOME/.vimrc
 ln -s .vim $HOME/.vim
 mkdir -p $HOME/.config
