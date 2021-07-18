f48dbb061d29e7632f63b6ea21038e6d333e8606
Charlie Andrews <charlie@cwndr.ws>
2021-07-18 11:40:16 -0400

    Add neovim and zsh config to home.nix

--- nix/home.nix
+++ nix/home.nix
@@ -1,8 +1,54 @@
 { config, pkgs, ... }:
 
 {
-  home.packages = [
-    pkgs.neovim
+  home.packages = with pkgs; [
+    tmux
+    ripgrep
+    zsh
   ];
-  programs.home-manager.enable = true;
+
+  programs = {
+    zsh = {
+      enable = true;
+      enableAutosuggestions = true;
+      enableCompletion = true;
+
+      oh-my-zsh = {
+        enable = true;
+        plugins = [ "git" "sudo" ];
+        theme = "bureau";
+      };
+
+      localVariables = {
+        EDITOR = "vim";
+      };
+    };
+
+    neovim = {
+      enable = true;
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
+
+      plugins = with pkgs.vimPlugins; [
+        vim-airline
+        gruvbox
+        vim-nix
+      ];
+
+      extraConfig = ''
+        set background=dark
+        colorscheme gruvbox
+        set expandtab
+        set tabstop=2
+        set shiftwidth=2
+        set number
+        set relativenumber
+      '';
+    };
+
+    home-manager = {
+      enable = true;
+    };
+  };
 }
