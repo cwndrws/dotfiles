9859343a7d9b6d1125e79dacc359d0e7a707a40f
Charlie Andrews <cwndrws@github.com>
2022-01-07 17:03:02 -0500

    Add neovim config to home manager

--- nix/home.nix
+++ nix/home.nix
@@ -1,5 +1,17 @@
 { config, pkgs, ... }:
 
+let
+  settings = pkgs.vimUtils.buildVimPlugin {
+    name = "neovim_settings";
+    src = pkgs.fetchFromGitHub {
+      owner = "cwndrws";
+      repo = "neovim_settings";
+      rev = "d3899d5bc74b35b33563ed7501895388325847a2";
+      sha256 = "0g08n89m579wiyravj2rh8k8hyfi4cq11gypxsry5w1dv99ks403";
+    };
+  };
+
+in
 {
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
@@ -18,4 +30,26 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+  programs.neovim = {
+    enable = true;
+    plugins =
+    plug.plugins = with pkgs.vimPlugins; [
+      settings
+      ale
+      fzf-vim
+      vim-dispatch
+      vim-fugitive
+      typescript-vim
+      vim-rails
+      gruvbox
+      vim-easymotion
+      emmet-vim
+      auto-pairs
+      vim-endwise
+      vim-surround
+      vim-test
+      vim-nix
+      airline
+    ];
+  };
 }
