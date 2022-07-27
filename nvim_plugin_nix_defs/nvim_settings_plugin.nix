e25332a78b071bdcf841d0a711ecf00c72e8f1f5
Charlie Andrews <cwndrws@github.com>
2022-07-27 17:43:20 -0400

    Bump nvim settings plugin version

--- nvim_plugin_nix_defs/nvim_settings_plugin.nix
+++ nvim_plugin_nix_defs/nvim_settings_plugin.nix
@@ -4,7 +4,7 @@ pkgs.vimUtils.buildVimPlugin {
   src = pkgs.fetchFromGitHub {
     owner = "cwndrws";
     repo = "neovim_settings";
-    rev = "657fa07d4977e62d0fd0c2e76872507e21e5d771";
-    sha256 = "0g2ylyyq8dxsadx71pkfg1naw9cppd3l6lpsvv01qv1vwkba872l";
+    rev = "63c6d8e74339f9a9850bb592d4b61df41f80f075";
+    sha256 = "16n69n8ifv4slp3q10b6waaw2cn6cing15zbkkkcccgqylwi10qr";
   };
 }
