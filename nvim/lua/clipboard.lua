ec2881436cf52db7d4cd792e0b7f9c85949514c2
Charlie Andrews <charlie@cwndr.ws>
2024-12-10 16:08:34 -0500

    Update clipboard code

--- nvim/lua/clipboard.lua
+++ nvim/lua/clipboard.lua
@@ -1,5 +1,5 @@
-local function init()
-  vim.g.clipboard = {
+local function osc_52_clipboard()
+   return {
     name = 'OSC 52',
     copy = {
       ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
@@ -9,7 +9,31 @@ local function init()
       ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
       ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
     },
+  } 
+end
+
+local function tmux_buffer_clipboard()
+  return {
+    name = 'Tmux Buffer',
+    copy = {
+      ['+'] = {'tmux', 'load-buffer', '-'},
+      ['*'] = {'tmux', 'load-buffer', '-'},
+    },
+    paste = {
+      ['+'] = {'tmux', 'save-buffer', '-'},
+      ['*'] = {'tmux', 'save-buffer', '-'},
+    },
   }
+end
+
+local function init()
+  if vim.env.TMUX == nil then
+    print("using OSC 52 clipboard")
+    vim.g.clipboard = osc_52_clipboard()
+  else
+    print("using tmux clipboard")
+    vim.g.clipboard = tmux_buffer_clipboard()
+  end
 end
 
 return {
