251a0e43bf018abad22b387a55d8b28754309928
Charlie Andrews <charlie@cwndr.ws>
2025-01-06 14:12:55 -0500

    Add autocommand for trimming whitespace on save

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -1,14 +1,20 @@
 local function create_lsp_format_on_save_autocommand()
   local function lsp_format_on_save()
-    vim.lsp.buf.format() 
+    vim.lsp.buf.format()
   end
 
-  vim.api.nvim_create_autocmd({"BufWritePre"}, {
-    callback = lsp_format_on_save
+  vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    callback = lsp_format_on_save,
   })
 end
 
-local function init ()
+local function create_whitespace_trim_on_save_autocommand()
+  vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    command = ':%s/\\s\\+$//e',
+  })
+end
+
+local function init()
   create_lsp_format_on_save_autocommand()
 end
 
