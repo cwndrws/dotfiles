18506d3309541399bf0badbc44544c9de0f287db
Charlie Andrews <charlie@cwndr.ws>
2025-01-31 12:51:30 -0500

    Setup nvim clipboard to work with osc52 properly

--- nvim/lua/clipboard.lua
+++ nvim/lua/clipboard.lua
@@ -1,5 +1,7 @@
-local function osc_52_clipboard()
-   return {
+local function init()
+  vim.o.clipboard = 'unnamedplus'
+
+  vim.g.clipboard = {
     name = 'OSC 52',
     copy = {
       ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
@@ -9,31 +11,7 @@ local function osc_52_clipboard()
       ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
       ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
     },
-  } 
-end
-
-local function tmux_buffer_clipboard()
-  return {
-    name = 'Tmux Buffer',
-    copy = {
-      ['+'] = {'tmux', 'load-buffer', '-'},
-      ['*'] = {'tmux', 'load-buffer', '-'},
-    },
-    paste = {
-      ['+'] = {'tmux', 'save-buffer', '-'},
-      ['*'] = {'tmux', 'save-buffer', '-'},
-    },
   }
-end
-
-local function init()
-  if vim.env.TMUX == nil then
-    print("using OSC 52 clipboard")
-    vim.g.clipboard = osc_52_clipboard()
-  else
-    print("using tmux clipboard")
-    vim.g.clipboard = tmux_buffer_clipboard()
-  end
 end
 
 return {
