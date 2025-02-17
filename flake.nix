4d59839fb8205caa1ae23c19696a8246b495f2fd
Charlie Andrews <charlie@cwndr.ws>
2025-02-16 20:27:35 -0500

    Squashed commit of the following:
    commit e5181c71718c40ba9a1cdad6f9d346de12ce6795
    Author: Charlie Andrews <charlie@cwndr.ws>
    Date:   Thu Feb 6 14:28:37 2025 -0500
    
        Update flake
    
    commit 9e94400279e7cd9c9a6185e3f6d41afad810eec5
    Author: Charlie Andrews <charlie@cwndr.ws>
    Date:   Thu Feb 6 14:08:11 2025 -0500
    
        try simplest setup
    
    commit 79cb76d31c40bc3f94edf55fefb5d25f4c1d5889
    Author: Charlie Andrews <charlie@cwndr.ws>
    Date:   Thu Feb 6 13:44:55 2025 -0500
    
        Add flake contents to dotfiles

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "cwndrws dotfiles";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+  };
+
+  outputs = { self, nixpkgs, home-manager }:
+    let
+      systems = [
+				"x86_64-linux" # 64-bit Intel/AMD Linux
+				"aarch64-linux" # 64-bit ARM Linux
+				"x86_64-darwin" # 64-bit Intel macOS
+				"aarch64-darwin" # 64-bit ARM macOS
+			];
+
+			forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f {
+				pkgs = import nixpkgs { inherit system; };
+			});
+    in
+    {
+			packages = forAllSystems({pkgs}: {
+				default = home-manager.defaultPackage.${pkgs.system};
+				homeConfigurations = {
+					username = import ./user.nix;
+					home = import ./home.nix;
+				};
+			});
+    };
+}
