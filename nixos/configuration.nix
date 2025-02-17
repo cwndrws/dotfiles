b0cf76ae6da7a8ec6a25ff1da174782675f80772
Charlie Andrews <charlieandrews.cwa@gmail.com>
2025-02-16 20:30:34 -0500

    Add ghostty and signal to system packages

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -79,6 +79,8 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
+    signal-desktop
+    ghostty
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
